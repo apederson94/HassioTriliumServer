53535b19df738742dba227fb35dc1b7d5fbb0e14
Austin Pederson <austinpederson@pm.me>
2021-01-18 18:23:28 -0800

    added TLS stuff. as per the wiki

--- start.sh
+++ start.sh
@@ -1,9 +1,23 @@
+#!/user/bin/with-contenv bashio
+cert=$(bashio::config 'certfile')
+privatekey=$(bashio::config 'privatekey')
 mkdir /trilium && cd /trilium
 mkdir versions/ && cd versions/
 mkdir 0.45.8/ && cd 0.45.8/
 
 git clone https://github.com/zadam/trilium.git && cd trilium
 
+npm set cafile $(bashio::config 'certfile')
+
 npm install
 
-nohup node src/www
+export TRILIUM_DATA_DIR=/data/trilium/data/
+
+mkdir /data && mkdir /data/trilium && mkdir /data/trilium/data
+
+touch /data/trilium/data/config.ini
+
+printf "[Network]\nport=8080\nhttps=true\ncertPath=$certfile\nkeyPath=$privatekey"
+
+nohup node src/www &
+
